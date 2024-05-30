5f275c20ad1261a6555bd4e1ee31ce0004c2844c
진의영 <jineuiyoung@jin-uiyeong-ui-MacBookPro.local>
2024-05-30 11:05:26 +0200

    user

--- User.cpp
+++ User.cpp
@@ -5,6 +5,8 @@
 //-------------------------------------------------------- System Includes
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 //------------------------------------------------------ Personal Includes
@@ -13,20 +15,59 @@ using namespace std;
 //----------------------------------------------------------------- PUBLIC
 
 //--------------------------------------------------------- Public Methods
-string User::getScore ( )
+int User::getScore()
 {
     return score;
 } //----- End of getScore
 
-User::User ( )
+User::User()
 {
 #ifdef MAP
     cout << "Call to the constructor of <User>" << endl;
 #endif
 } //----- End of User
 
+bool User::evaluateReliability(const std::vector<std::string>& measurements)
+{
+    int anomalyCount = 0;
+    for (const string& measurement : measurements)
+    {
+        stringstream ss(measurement);
+        string timestamp, sensorID, attributeID, valueStr;
+        getline(ss, timestamp, ';');
+        getline(ss, sensorID, ';');
+        getline(ss, attributeID, ';');
+        getline(ss, valueStr, ';');
 
+        try
+        {
+            float value = stof(valueStr);
+            if (value < 0 || value > 1000) 
+            {
+                anomalyCount++;
+            }
+        }
+        catch (invalid_argument& e)
+        {
+            anomalyCount++;
+        }
+    }
+
+    isReliable = (anomalyCount <= measurements.size() * 0.1);
+    return isReliable;
+}
+
+//-------------------------------------------- Constructors - Destructor
 User::~User ( )
+{
+#ifdef MAP
+    cout << "Call to the destructor of <User>" << endl;
+#endif
+    score = 0;
+    isReliable = true;
+} //----- End of ~User
+
+User::~User()
 {
 #ifdef MAP
     cout << "Call to the destructor of <User>" << endl;
